2e4a07c509c72b9b1bff690a7b51a70d6cd9131a
Cooper Saye <coopersaye@gmail.com>
2021-08-01 14:17:40 -0700

    Import palette data.

---
+++ data/palettes.js
@@ -0,0 +1,42 @@
+export const palettes = [
+  {
+    name: "Grayscale",
+    colors: ['#dfdfdf', '#9f9f9f', '#606060', '#202020']
+  },
+  {
+    name: "Gameboy",
+    colors: ['#e0f8d0', '#88c070', '#346856', '#081820']
+  },
+  {
+    name: "Rustic",
+    colors: ['#edb4a1', '#a96868', '#764462', '#2c2137']
+  },
+  {
+    name: "Mist",
+    colors: ['#c4f0c2', '#5ab9a8', '#1e606e', '#2d1b00']
+  },
+  {
+    name: "Ice Cream",
+    colors: ['#fff6d3', '#f9a875', '#eb6b6f', '#7c3f58']
+  },
+  {
+    name: "Kirokaze",
+    colors: ['#e2f3e4', '#94e344', '#46878f', '#332c50']
+  },
+  {
+    name: "Wish",
+    colors: ['#8be5ff', '#608fcf', '#7550e8', '#622e4c']
+  },
+  {
+    name: "Nostalgia",
+    colors: ['#d0d058', '#a0a840', '#708028', '#405010']
+  },
+  {
+    name: "Nintendo",
+    colors: ['#f7e7c6', '#d68e49', '#a63725', '#331e50']
+  },
+  {
+    name: "Heart",
+    colors: ['#ebe5ce', '#ff4589', '#a64777', '#3e2653']
+  }
+];
