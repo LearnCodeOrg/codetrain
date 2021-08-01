f5e940bc2f271e646799257df1be8305790c1f9a
Cooper Saye <coopersaye@gmail.com>
2021-08-01 13:46:23 -0700

    Create color selector.

--- components/Sprites.js
+++ components/Sprites.js
@@ -1,9 +1,35 @@
+import { useState } from 'react';
+
 import styles from '../styles/Sprites.module.css';
 
+const defaultColors = ['#dfdfdf', '#9f9f9f', '#606060', '#202020'];
+
 export default function Sprites() {
+  const [colors, setColors] = useState(defaultColors);
+  const [currColor, setCurrColor] = useState(0);
+
   return (
     <div>
-      <h1>Colors</h1>
+      <div>
+        <h1>Colors</h1>
+        <div className={styles.tilegrid}>
+          {
+            colors.map((color, i) =>
+              <div
+                onClick={() => setCurrColor(i)}
+                className={
+                  currColor === i ?
+                  `${styles.tile} ${styles.selected}` :
+                  styles.tile
+                }
+                key={i}
+                style={{ background: color }}
+              >
+              </div>
+            )
+          }
+        </div>
+      </div>
       <h1>Sprites</h1>
     </div>
   );
