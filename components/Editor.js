20a644c9e5d3bef6a95fdfe4a029c9a03bfbe152
Cooper Saye <coopersaye@gmail.com>
2021-08-01 15:58:17 -0700

    Clean up Editor component.

--- components/Editor.js
+++ components/Editor.js
@@ -2,55 +2,26 @@ import AceEditor from 'react-ace';
 
 import { useState } from 'react';
 
-import 'ace-builds/webpack-resolver.js';
 import 'ace-builds/src-noconflict/mode-javascript.js';
 import 'ace-builds/src-noconflict/theme-monokai.js';
 import styles from '../styles/Editor.module.css';
 
-// props to be applied to all editors
-const editorProps = {
-  mode: "javascript",
-  theme: "monokai",
-  wrapEnabled: true,
-  showPrintMargin: false,
-  width: "100%",
-  height: "250px"
-};
-
 export default function Editor() {
-  const [startCode, setStartCode] = useState('');
-  const [updateCode, setUpdateCode] = useState('');
-  const [drawCode, setDrawCode] = useState('');
+  const [code, setCode] = useState('');
 
   return (
-    <div className={styles.container}>
-      <div className={styles.panel}>
-        <h1>Start</h1>
-        <AceEditor
-          name="start-editor"
-          value={startCode}
-          onChange={val => setStartCode(val)}
-          { ...editorProps }
-        />
-      </div>
-      <div className={styles.panel}>
-        <h1>Update</h1>
-        <AceEditor
-          name="update-editor"
-          value={updateCode}
-          onChange={val => setUpdateCode(val)}
-          { ...editorProps }
-        />
-      </div>
-      <div className={styles.panel}>
-        <h1>Draw</h1>
-        <AceEditor
-          name="draw-editor"
-          value={drawCode}
-          onChange={val => setDrawCode(val)}
-          { ...editorProps }
-        />
-      </div>
+    <div>
+      <AceEditor
+        name="start-editor"
+        value={code}
+        onChange={val => setCode(val)}
+        mode="javascript"
+        theme="monokai"
+        wrapEnabled={true}
+        showPrintMargin={false}
+        tabSize={2}
+        setOptions={{ useWorker: false }}
+      />
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Index() {
   return (
     <div className={styles.container}>
       <Sprites />
+      <Editor />
     </div>
   );
 }
