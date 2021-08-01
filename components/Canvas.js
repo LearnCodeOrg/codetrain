e5ee9796a3eabc0700a4e4a261d78ef6982aa8cd
Cooper Saye <coopersaye@gmail.com>
2021-08-01 16:17:11 -0700

    Improve canvas styling.

--- components/Canvas.js
+++ components/Canvas.js
@@ -1,16 +1,12 @@
 import styles from '../styles/Canvas.module.css';
 
-// canvas width and height
-const width = 256;
-const height = 256;
-
 export default function Canvas() {
   return (
     <div className={styles.container}>
       <canvas
-        className={styles.canvas}
-        width={width}
-        height={height}
+        className={styles.screen}
+        width={256}
+        height={256}
       />
     </div>
   );
