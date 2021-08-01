fb40283be25806ff043ed0c6283ca55e4b860688
Cooper Saye <coopersaye@gmail.com>
2021-08-01 13:49:56 -0700

    Create sprite selector.

--- components/Sprites.js
+++ components/Sprites.js
@@ -3,10 +3,13 @@ import { useState } from 'react';
 import styles from '../styles/Sprites.module.css';
 
 const defaultColors = ['#dfdfdf', '#9f9f9f', '#606060', '#202020'];
+const defaultSprites = Array(16).fill(Array(64).fill(0));
 
 export default function Sprites() {
   const [colors, setColors] = useState(defaultColors);
   const [currColor, setCurrColor] = useState(0);
+  const [sprites, setSprites] = useState(defaultSprites);
+  const [currSprite, setCurrSprite] = useState(0);
 
   return (
     <div>
@@ -30,7 +33,26 @@ export default function Sprites() {
           }
         </div>
       </div>
-      <h1>Sprites</h1>
+      <div>
+        <h1>Sprites</h1>
+        <div className={styles.tilegrid}>
+          {
+            sprites.map((sprite, i) =>
+              <div
+                onClick={() => setCurrSprite(i)}
+                className={
+                  currSprite === i ?
+                  `${styles.tile} ${styles.selected}` :
+                  styles.tile
+                }
+                key={i}
+                style={{ background: colors[sprite[0]] }}
+              >
+              </div>
+            )
+          }
+        </div>
+      </div>
     </div>
   );
 }
