47076cc517e2a34db721e5097804176b6617de94
Cooper Saye <coopersaye@gmail.com>
2021-08-01 17:11:45 -0700

    Create power button.

--- components/Game.js
+++ components/Game.js
@@ -1,10 +1,23 @@
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import StopIcon from '@material-ui/icons/Stop';
 import Button from '@material-ui/core/Button';
+
+import { useState } from 'react';
 
 import styles from '../styles/Game.module.css';
 
 export default function Game() {
+  const [playing, setPlaying] = useState(false);
+
   return (
     <div className={styles.container}>
+      <Button
+        className={`${styles.button} ${styles.powerbutton}`}
+        variant="contained"
+        onClick={() => setPlaying(!playing)}
+      >
+        {playing ? <StopIcon /> : <PlayArrowIcon />}
+      </Button>
       <canvas
         className={styles.screen}
         width={256}
