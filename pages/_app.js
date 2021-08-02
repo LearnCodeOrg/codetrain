24fe5c649086619eefef14dc5bf2dea2ca98ffba
Cooper Saye <coopersaye@gmail.com>
2021-08-01 17:08:17 -0700

    Import favicon.

--- pages/_app.js
+++ pages/_app.js
@@ -21,6 +21,10 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>Codetrain</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <Component {...pageProps} />
     </>
