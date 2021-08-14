3faaeb06eb01aa2b6095ea78d3c66af2e159240b
Cooper Saye <coopersaye@gmail.com>
2021-08-14 11:41:36 -0700

    Prepare for deploy.

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDu_VbW1bzdLlY_aFsQnVQ_TXtczrW1unc",
+  authDomain: "codetrain-org.firebaseapp.com",
+  projectId: "codetrain-org",
+  storageBucket: "codetrain-org.appspot.com",
+  messagingSenderId: "755030431905",
+  appId: "1:755030431905:web:2eeb48ccd6e985a338f8e0"
+};
