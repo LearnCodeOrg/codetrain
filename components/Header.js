c279b50125dbeadc2c86d336674705cc31c6129f
Cooper Saye <coopersaye@gmail.com>
2021-08-14 22:37:56 -0700

    Update header.

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,12 @@
+import Link from 'next/link';
 import Image from 'next/image';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import ExploreIcon from '@material-ui/icons/Explore';
 
 import firebase from 'firebase/app';
 
@@ -13,17 +21,52 @@ export default function Header() {
 
   return (
     <div className={styles.container}>
-      <Image src="/logo.png" height="48" width="48" />
+      <Link href="/">
+        <a className={styles.icon}>
+          <Image src="/logo.png" height="48" width="48" />
+        </a>
+      </Link>
       <h1>Codetrain</h1>
       <span className={styles.flexfill} />
-      {
-        firebase.auth().currentUser &&
-        <p>Signed in as {firebase.auth().currentUser.displayName}</p>
-      }
+      <Link href="/learn">
+        <a>
+          <Tooltip title="Learn" arrow>
+            <IconButton>
+              <CheckCircleIcon />
+            </IconButton>
+          </Tooltip>
+        </a>
+      </Link>
+      <Link href="/create">
+        <a>
+          <Tooltip title="Create" arrow>
+            <IconButton>
+              <AddCircleIcon />
+            </IconButton>
+          </Tooltip>
+        </a>
+      </Link>
+      <Link href="/explore">
+        <a>
+          <Tooltip title="Explore" arrow>
+            <IconButton>
+              <ExploreIcon />
+            </IconButton>
+          </Tooltip>
+        </a>
+      </Link>
       {
         firebase.auth().currentUser ?
-        <button onClick={() => firebase.auth().signOut()}>Sign Out</button> :
-        <button onClick={signInWithGoogle}>Sign in with Google</button>
+        <Tooltip title="Sign Out" arrow>
+          <IconButton onClick={() => firebase.auth().signOut()}>
+            <ExitToAppIcon />
+          </IconButton>
+        </Tooltip> :
+        <Tooltip title="Sign In" arrow>
+          <IconButton onClick={signInWithGoogle}>
+            <PersonOutlineIcon />
+          </IconButton>
+        </Tooltip>
       }
     </div>
   );
