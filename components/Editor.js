29b75fcebb11c79a97c7c0c049704a3e411096e4
Cooper Saye <coopersaye@gmail.com>
2021-08-16 14:02:43 -0700

    Create Editor component.

---
+++ components/Editor.js
@@ -0,0 +1,18 @@
+import AceEditor from 'react-ace';
+
+import 'ace-builds/src-noconflict/mode-javascript.js';
+import 'ace-builds/src-noconflict/theme-monokai.js';
+
+export default function Editor(props) {
+  return (
+    <AceEditor
+      mode="javascript"
+      theme="monokai"
+      wrapEnabled={true}
+      showPrintMargin={false}
+      tabSize={2}
+      setOptions={{ useWorker: false }}
+      {...props}
+    />
+  );
+}
