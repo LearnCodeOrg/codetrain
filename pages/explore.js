712ea247f8cc5973e020e55bf531d641eccce1dc
Cooper Saye <coopersaye@gmail.com>
2021-08-16 19:43:35 -0700

    Retrieve project data on client side.

--- pages/explore.js
+++ pages/explore.js
@@ -1,33 +1,32 @@
 import Project from '../components/cards/Project.js';
 
+import { useEffect, useState } from 'react';
 import firebase from 'firebase/app';
 
 import styles from '../styles/Explore.module.css';
 
-export default function Challenges(props) {
-  const { data } = props;
+export default function Explore() {
+  const [projects, setProjects] = useState(undefined);
+
+  async function getProjects() {
+    // get and set projects data
+    const projectsRef = firebase.firestore().collection('projects');
+    const projectsDocs = (await projectsRef.get()).docs;
+    setProjects(projectsDocs.map(doc => ({ ...doc.data(), id: doc.id })));
+  }
+
+  // get projects on start
+  useEffect(() => getProjects(), []);
+
+  if (!projects) return <div>Loading...</div>;
 
   return (
     <div className={styles.projects}>
       {
-        data.map(project =>
+        projects.map(project =>
           <Project {...project} key={project.id} />
         )
       }
     </div>
   );
-}
-
-export async function getStaticProps() {
-  // get challenge data
-  const projectsRef = firebase.firestore().collection('projects');
-  const projectsDocs = await projectsRef.get();
-  const projectsData = projectsDocs.docs.map(doc => (
-    { ...doc.data(), id: doc.id }
-  ));
-  // return challenge data
-  return {
-    props: { data: projectsData },
-    revalidate: 60
-  };
 }
