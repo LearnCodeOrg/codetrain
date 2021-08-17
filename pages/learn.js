068bf2e3877e59b338c64bf2afec134b435e0460
Cooper Saye <coopersaye@gmail.com>
2021-08-16 19:39:55 -0700

    Retrieve challenge data on client side.

--- pages/learn.js
+++ pages/learn.js
@@ -1,18 +1,31 @@
 import Challenge from '../components/cards/Challenge.js';
 import Link from 'next/link';
 
+import { useEffect, useState } from 'react';
 import firebase from 'firebase/app';
 
 import styles from '../styles/Learn.module.css';
 
 export default function Learn(props) {
-  const { data } = props;
+  const [challenges, setChallenges] = useState(undefined);
+
+  async function getChallenges() {
+    // get and set challenges data
+    const challengesRef = firebase.firestore().collection('challenges');
+    const challengesDocs = (await challengesRef.get()).docs;
+    setChallenges(challengesDocs.map(doc => ({ ...doc.data(), id: doc.id })));
+  }
+
+  // get challenges on start
+  useEffect(() => getChallenges, []);
+
+  if (!challenges) return <div>Loading...</div>;
 
   return (
     <div className={styles.container}>
       <div className={styles.challenges}>
         {
-          data.map(challenge =>
+          challenges.map(challenge =>
             <Challenge {...challenge} key={challenge.id} />
           )
         }
@@ -22,18 +35,4 @@ export default function Learn(props) {
       </Link>
     </div>
   );
-}
-
-export async function getStaticProps() {
-  // get challenge data
-  const challengesRef = firebase.firestore().collection('challenges');
-  const challengesDocs = await challengesRef.get();
-  const challengesData = challengesDocs.docs.map(doc => (
-    { ...doc.data(), id: doc.id }
-  ));
-  // return challenge data
-  return {
-    props: { data: challengesData },
-    revalidate: 60
-  };
 }
