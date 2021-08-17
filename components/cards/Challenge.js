0c68272f017f836cf2bab3154832ce7bccfaab43
Cooper Saye <coopersaye@gmail.com>
2021-08-16 18:24:47 -0700

    Create challenge card.

---
+++ components/cards/Challenge.js
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+import styles from '../../styles/Challenge.module.css';
+
+export default function Challenge(props) {
+  const { id, title, description } = props;
+
+  return (
+    <div className={styles.container}>
+      <Link href={`/challenge/${id}`}>
+        <a>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </a>
+      </Link>
+    </div>
+  );
+}
