b4597fa9a73904bcc43b8510f4cbe96f73127183
Cooper Saye <coopersaye@gmail.com>
2021-08-17 22:50:34 -0700

    Create homepage.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,35 @@
+import Image from 'next/image';
+import Button from '@material-ui/core/Button';
+
+import firebase from 'firebase/app';
+import signInWithGoogle from '../util/signInWithGoogle.js';
+
 import styles from '../styles/pages/Index.module.css';
 
 export default function Index() {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.center}>
+        <div className={styles.title}>
+          <h1>Codetrain</h1>
+          <Image src="/logo.png" height="48" width="48" />
+        </div>
+        {
+          firebase.auth().currentUser ?
+          <Button
+            onClick={() => firebase.auth().signOut()}
+            variant="contained"
+          >
+            Sign Out
+          </Button> :
+          <Button
+            onClick={signInWithGoogle}
+            variant="contained"
+          >
+            Sign In
+          </Button>
+        }
+      </div>
     </div>
   );
 }
