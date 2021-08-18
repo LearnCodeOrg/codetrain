a925651eb8cee50868354df5f72be1dcac0dd837
Cooper Saye <coopersaye@gmail.com>
2021-08-17 21:38:47 -0700

    Create Loading component.

---
+++ components/Loading.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Loading.module.css';
+
+export default function Loading() {
+  return (
+    <div className={styles.container}>
+      <h1>Loading...</h1>
+    </div>
+  );
+}
