ed57865e084d2060437bb4fafb95686814d10bb3
Cooper Saye <coopersaye@gmail.com>
2021-09-16 22:37:24 -0700

    Improve styling.

--- pages/_app.js
+++ pages/_app.js
@@ -26,6 +26,7 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
         <link rel="manifest" href="/manifest.json" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;900&display=swap" rel="stylesheet" />
       </Head>
       <Header />
       <div style={{ height: 60 }} />
