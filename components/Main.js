66956dc85f11c7c28d945e76d87aa42a777c1edd
Cooper Saye <coopersaye@gmail.com>
2021-09-23 20:25:34 -0700

    Listen for user doc.

--- components/Main.js
+++ components/Main.js
@@ -2,6 +2,28 @@ import Header from './Header.js';
 
 import firebase from 'firebase/app';
 import { useEffect, useState } from 'react';
+import { useDocument } from 'react-firebase-hooks/firestore';
+
+function MainAuthed(props) {
+  const { Component, pageProps } = props;
+
+  // get current user
+  const uid = firebase.auth().currentUser.uid;
+  const userRef = firebase.firestore().collection('users').doc(uid);
+  const [userDoc] = useDocument(userRef);
+
+  // get auth state from user doc
+  const authed = userDoc === undefined ? false : userDoc.exists ? true : null;
+
+  return (
+    <>
+      <Header authed={authed} />
+      <div style={{ height: 60 }} />
+      <Component authed={authed} {...pageProps} />
+    </>
+  );
+}
+
 export default function Main(props) {
   const { Component, pageProps } = props;
 
@@ -16,6 +38,8 @@ export default function Main(props) {
   }, []);
 
   return (
+    authed ?
+    <MainAuthed {...props} /> :
     <>
       <Header />
       <div style={{ height: 60 }} />
