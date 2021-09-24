07c7bd77d1126f5b0f93afef2fee4bf4e166e88e
Cooper Saye <coopersaye@gmail.com>
2021-09-23 20:27:12 -0700

    Verify username.

--- components/Header.js
+++ components/Header.js
@@ -20,6 +20,22 @@ export default function Header(props) {
 
   // attempts to create user
   async function createUser() {
+    const username = window.prompt('Enter a username:');
+    // verify username
+    if (!username) {
+      alert("Please enter a username.");
+      return;
+    }
+    // verify username chars
+    if (!/^[A-Za-z0-9_]+$/.test(username)) {
+      alert("Username can only contain alphanumeric characters and underscore.");
+      return;
+    }
+    // verify username length
+    if (username.length < 2 || username.length > 16) {
+      alert("Username must be between 2 and 16 characters.");
+      return;
+    }
     // create user documents
     const { uid, photoURL } = firebase.auth().currentUser;
     const userRef = firebase.firestore().collection('users').doc(uid);
