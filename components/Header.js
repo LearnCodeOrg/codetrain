d3fe6f14049781a2a852548ee1325f0d66a2350c
Cooper Saye <coopersaye@gmail.com>
2021-09-26 16:01:38 -0700

    Fix header display.

--- components/Header.js
+++ components/Header.js
@@ -17,48 +17,51 @@ import createUser from '../util/createUser.js';
 import styles from '../styles/components/Header.module.css';
 
 export default function Header(props) {
-  const { authed } = props;
+  const { username } = props;
 
   return (
-    <div className={styles.container}>
-      <Link href="/">
-        <a className={styles.icon}>
-          <Image src="/logo.png" height="48" width="48" alt="logo" />
-        </a>
-      </Link>
-      <h1>Codetrain</h1>
-      <span className="flexfill" />
-      <Link href="/">
-        <a className={styles.link}>Home</a>
-      </Link>
-      <Link href="/learn">
-        <a className={styles.link}>Learn</a>
-      </Link>
-      <Link href="/create">
-        <a className={styles.link}>Create</a>
-      </Link>
-      <Link href="/explore">
-        <a className={styles.link}>Explore</a>
-      </Link>
-      {
-        authed === null ?
-        <Tooltip title="Choose Username" arrow>
-          <IconButton onClick={createUser}>
-            <HelpOutlineIcon />
-          </IconButton>
-        </Tooltip> :
-        authed ?
-        <Tooltip title="Sign Out" arrow>
-          <IconButton onClick={() => firebase.auth().signOut()}>
-            <ExitToAppIcon />
-          </IconButton>
-        </Tooltip> :
-        <Tooltip title="Sign In" arrow>
-          <IconButton onClick={signInWithGoogle}>
-            <PersonOutlineIcon />
-          </IconButton>
-        </Tooltip>
-      }
-    </div>
+    <>
+      <div className={styles.container}>
+        <Link href="/">
+          <a className={styles.icon}>
+            <Image src="/logo.png" height="48" width="48" alt="logo" />
+          </a>
+        </Link>
+        <h1>Codetrain</h1>
+        <span className="flexfill" />
+        <Link href="/">
+          <a className={styles.link}>Home</a>
+        </Link>
+        <Link href="/learn">
+          <a className={styles.link}>Learn</a>
+        </Link>
+        <Link href="/create">
+          <a className={styles.link}>Create</a>
+        </Link>
+        <Link href="/explore">
+          <a className={styles.link}>Explore</a>
+        </Link>
+        {
+          username === null ?
+          <Tooltip title="Choose Username" arrow>
+            <IconButton onClick={createUser}>
+              <HelpOutlineIcon />
+            </IconButton>
+          </Tooltip> :
+          username ?
+          <Tooltip title="Sign Out" arrow>
+            <IconButton onClick={() => firebase.auth().signOut()}>
+              <ExitToAppIcon />
+            </IconButton>
+          </Tooltip> :
+          <Tooltip title="Sign In" arrow>
+            <IconButton onClick={signInWithGoogle}>
+              <PersonOutlineIcon />
+            </IconButton>
+          </Tooltip>
+        }
+      </div>
+      <div style={{ height: 60 }} />
+    </>
   );
 }
