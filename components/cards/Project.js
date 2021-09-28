b5c4847b8af8d903f62e8dcf7080491ad12eb308
Cooper Saye <coopersaye@gmail.com>
2021-09-27 22:34:51 -0700

    Create project cover canvas.

--- components/cards/Project.js
+++ components/cards/Project.js
@@ -1,10 +1,30 @@
+import { useEffect, useRef } from 'react';
+
 import styles from '../../styles/components/cards/Project.module.css';
+
+let canvas, ctx;
 
 export default function Project(props) {
   const { id, title, username } = props;
 
+  const canvasRef = useRef();
+
+  // on start
+  useEffect(() => {
+    // get cover canvas
+    canvas = canvasRef.current;
+    ctx = canvas.getContext('2d');
+  }, []);
+
   return (
     <div className={styles.container}>
+      <a href={`/projects/${id}`}>
+        <canvas
+          ref={canvasRef}
+          width={mapPixels}
+          height={mapPixels}
+        />
+      </a>
       <a href={`/projects/${id}`}>
         <h1>{title}</h1>
       </a>
