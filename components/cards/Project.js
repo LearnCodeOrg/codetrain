3bb2220e858315726644abda40c3fbc44d3916a7
Cooper Saye <coopersaye@gmail.com>
2021-09-27 22:35:54 -0700

    Implement cover sprite drawing.

--- components/cards/Project.js
+++ components/cards/Project.js
@@ -9,6 +9,24 @@ export default function Project(props) {
 
   const canvasRef = useRef();
 
+  // draws sprite to cover
+  function drawSprite(sprite, x, y) {
+    // for each pixel
+    for (let yp = 0; yp < spriteSize; yp++) {
+      for (let xp = 0; xp < spriteSize; xp++) {
+        // set fill color
+        const colorIndex = yp * spriteSize + xp;
+        const color = colors[sprite[colorIndex]];
+        ctx.fillStyle = color;
+        // get fill position
+        let xm = x + xp * pixelPixels;
+        let ym = y + yp * pixelPixels;
+        // fill pixel
+        ctx.fillRect(xm, ym, pixelPixels, pixelPixels);
+      }
+    }
+  }
+
   // on start
   useEffect(() => {
     // get cover canvas
