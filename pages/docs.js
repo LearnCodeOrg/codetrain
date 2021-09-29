a4c3c3408ed95da36936110857a6d1a2307eab93
Cooper Saye <coopersaye@gmail.com>
2021-09-28 20:14:37 -0700

    Create docs Section component.

--- pages/docs.js
+++ pages/docs.js
@@ -1,3 +1,21 @@
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+function Section(props) {
+  return (
+    <Accordion>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+        {props.title}
+      </AccordionSummary>
+      <AccordionDetails>
+        {props.children}
+      </AccordionDetails>
+    </Accordion>
+  );
+}
+
 export default function Docs() {
   return (
     <div>
