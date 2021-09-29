14f2160700b4b93d267354b174cc34a9293ab534
Cooper Saye <coopersaye@gmail.com>
2021-09-29 14:56:56 -0700

    Update homepage text.

--- pages/index.js
+++ pages/index.js
@@ -25,25 +25,19 @@ export default function Index() {
           }
         </div>
         <h2>What is Codetrain?</h2>
-        <p>Codetrain is an educational browser game engine. Our goal is to lower
-        the barrier of entry into scripted programming languages.</p>
-        <h2>What makes Codetrain special?</h2>
-        <p>We aim to teach JavaScript, a real-world programming language, in an
-        engaging, game-ified environment. Whether you are a programming expert or
-        a complete beginner, the Codetrain engine aims to provide just the right
-        amount of complexity abstraction to make creating fun.</p>
+        <p>Codetrain is a retro game engine in the browser.</p>
+        <h2>Who is Codetrain for?</h2>
+        <p>Everyone. Whether you&apos;re a beginner looking to learn more about
+        game development or an expert looking to challenge themselves in a
+        constrained environment, Codetrain provides a platform for you.</p>
         <h2>How can I get started?</h2>
-        <p>If you are a complete beginner, you can take a look at our Codetrain
-        engine tutorials, which will take you through the basics of the JavaScript
-        programming language on your way to creating your first games. If you are
-        more advanced and looking to jump straight into the engine, you can check
-        out our documentation.</p>
+        <p>Check out our <a href="/docs">docs</a> or jump straight into the
+        {' '}<a href="/create">engine</a>.</p>
         <h2>Can I contribute?</h2>
         <p>
           Codetrain is open source. Want to contribute? Find our GitHub repository
           {' '}
           <a
-            className="link"
             href="https://github.com/codeconvoy/codetrain"
             target="_blank"
             rel="noopener noreferrer"
