a4ed8b47ae9b302f30bafece071872fa24b3e848
Cooper Saye <coopersaye@gmail.com>
2021-09-28 20:17:21 -0700

    Convert docs content to sections.

--- pages/docs.js
+++ pages/docs.js
@@ -19,40 +19,48 @@ function Section(props) {
 export default function Docs() {
   return (
     <div>
-      <h1>Game Loop</h1>
-      start(): runs once at the start of the game<br />
-      update(): runs once a frame
-      <h1>Input</h1>
-      isKeyDown(key): returns whether given key is pressed<br />
-      isKey(key): returns whether given key was pressed in the last frame
-      Output
-      say(text): opens a dialogue box with given text closable with left click
-      <h1>Movement</h1>
-      move(dir): moves object one tile up, down, left, or right<br />
-      moveTiles(x, y): moves object by x, y in tiles<br />
-      movePixels(x, y): moves object by x, y in pixels<br />
-      getTilePosition(): returns an object containing x, y position in tiles<br />
-      getPixelPosition(): returns an object containing x, y position in pixels
-      <h1>Tiles</h1>
-      getTile(): returns index of tile at position<br />
-      setTile(tile): sets tile at position to index tile<br />
-      getTileAt(x, y): returns index of tile at x, y<br />
-      setTileAt(x, y, tile): sets tile at x, y to index tile
-      <h1>Utility</h1>
-      range(num): returns an array containing numbers up to num<br />
-      sleep(sec): returns a promise expiring in sec seconds
-      <h1>Variables</h1>
-      colors: array of color data<br />
-      tiles: array of tile sprite data<br />
-      objects: array of object sprite data<br />
-      background: array of active tile indexes<br />
-      gameObjects: array of active objects
-      <h1>Constants</h1>
-      mapSize: map size in tiles<br />
-      mapPixels: map size in pixels<br />
-      spriteSize: sprite size in sprite pixels<br />
-      spritePixels: sprite size in pixels<br />
-      pixelPixels: sprite pixel size in pixels
+      <Section title="Game Loop">
+        start(): runs once at the start of the game<br />
+        update(): runs once a frame
+      </Section>
+      <Section title="Input">
+        isKeyDown(key): returns whether given key is pressed<br />
+        isKey(key): returns whether given key was pressed in the last frame
+      </Section>
+      <Section title="Output">
+        say(text): opens a dialogue box with given text closable with left click
+      </Section>
+      <Section title="Movement">
+        move(dir): moves object one tile up, down, left, or right<br />
+        moveTiles(x, y): moves object by x, y in tiles<br />
+        movePixels(x, y): moves object by x, y in pixels<br />
+        getTilePosition(): returns an object containing x, y position in tiles<br />
+        getPixelPosition(): returns an object containing x, y position in pixels
+      </Section>
+      <Section title="Tiles">
+        getTile(): returns index of tile at position<br />
+        setTile(tile): sets tile at position to index tile<br />
+        getTileAt(x, y): returns index of tile at x, y<br />
+        setTileAt(x, y, tile): sets tile at x, y to index tile
+      </Section>
+      <Section title="Utility">
+        range(num): returns an array containing numbers up to num<br />
+        sleep(sec): returns a promise expiring in sec seconds
+      </Section>
+      <Section title="Variables">
+        colors: array of color data<br />
+        tiles: array of tile sprite data<br />
+        objects: array of object sprite data<br />
+        background: array of active tile indexes<br />
+        gameObjects: array of active objects
+      </Section>
+      <Section title="Constants">
+        mapSize: map size in tiles
+        mapPixels: map size in pixels
+        spriteSize: sprite size in sprite pixels
+        spritePixels: sprite size in pixels
+        pixelPixels: sprite pixel size in pixels
+      </Section>
     </div>
   )
 }
