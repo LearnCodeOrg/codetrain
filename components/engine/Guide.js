9f93d50d5f383a4208152881ee942bc6c61573b8
Cooper Saye <coopersaye@gmail.com>
2021-09-28 20:06:48 -0700

    Remove Guide component.

--- components/engine/Guide.js
+++
@@ -1,9 +0,0 @@
-import styles from '../../styles/components/engine/Guide.module.css';
-
-export default function Guide() {
-  return (
-    <div className={styles.container}>
-      <h1>Guide</h1>
-    </div>
-  );
-}
