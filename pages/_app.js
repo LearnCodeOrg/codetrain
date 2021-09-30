2c874ad1ec1065edf79dc737b1abd810b6c62b34
Cooper Saye <coopersaye@gmail.com>
2021-09-29 22:08:56 -0700

    Update site meta.

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,11 @@ import { useEffect, useState } from 'react';
 
 import '../styles/globals.css';
 
+// meta
+const title = "Codetrain";
+const description = "A retro game engine in the browser.";
+const image = "https://codetrain.org/cover.png";
+
 // initialize firebase
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
@@ -19,12 +24,26 @@ export default function App(props) {
     <>
       <Head>
         <title>Codetrain</title>
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content={description} />
+        {/* links */}
         <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
         <link rel="manifest" href="/manifest.json" />
+        <link href="https://codetrain.org" rel="canonical" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;900&display=swap" rel="stylesheet" />
+        {/* open graph */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://codetrain.org" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+        {/* twitter */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@csaye_" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
       </Head>
       <Main {...props} />
     </>
