f8798718361473563a8ca9030ad688666c654096
Cooper Saye <coopersaye@gmail.com>
2021-09-30 20:04:18 -0700

    Create homepage accordion.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Image from 'next/image';
+import Accordion from '../components/Accordion';
 
 import firebase from 'firebase/app';
-import signInWithGoogle from '../util/signInWithGoogle.js';
+import signInWithGoogle from '../util/signInWithGoogle';
 
 import styles from '../styles/pages/Index.module.css';
 
@@ -13,6 +14,27 @@ export default function Index() {
           <Image src="/logo.png" height="48" width="48" alt="logo" />
           <h1>Codetrain</h1>
         </div>
+        <div className={styles.accordion}>
+          <Accordion title="What is Codetrain?">
+            Codetrain is a retro game engine in the browser.
+          </Accordion>
+          <Accordion title="Who is Codetrain for?">
+            Everyone. Whether you&apos;re a beginner looking to learn more about
+            game development or an expert looking to challenge themselves in a
+            constrained environment, Codetrain provides a platform for you.
+          </Accordion>
+          <Accordion title="How can I get started?">
+            Check out our <a href="/docs">docs</a> or jump straight into the {/* eslint-disable-line @next/next/no-html-link-for-pages */}
+            {' '}<a href="/create">engine</a>. {/* eslint-disable-line @next/next/no-html-link-for-pages */}
+          </Accordion>
+          <Accordion title="Can I contribute?">
+            Codetrain is open source. Want to contribute? Find our GitHub
+            {' '}repository <a
+              href="https://github.com/codeconvoy/codetrain"
+              target="_blank"
+              rel="noopener noreferrer"
+            >here</a>.
+          </Accordion>
         </div>
       </div>
     </div>
