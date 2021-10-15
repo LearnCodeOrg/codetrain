b3893c5bb1c9cff4538d524da75f8b13c4cd8abf
Cooper Saye <coopersaye@gmail.com>
2021-10-14 23:25:37 -0700

    Restructure docs page.

--- pages/docs.js
+++ pages/docs.js
@@ -39,196 +39,198 @@ export default function Docs() {
   return (
     <div className={styles.container}>
       <Header />
-      <Drawer
-        sx={{
-          width: 256,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
+      <div className={styles.main}>
+        <Drawer
+          sx={{
             width: 256,
-            boxSizing: 'border-box',
-            top: 60,
-            zIndex: 0
-          }
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        <List>
-          <a href="#gameloop">
-            <ListItem button>
-              <LoopIcon sx={{ marginRight: 2 }} />
-              <ListItemText primary="Game Loop" />
-            </ListItem>
-          </a>
-          <a href="#output">
-            <ListItem button>
-              <TextsmsIcon sx={{ marginRight: 2 }} />
-              <ListItemText primary="Output" />
-            </ListItem>
-          </a>
-          <a href="#input">
-            <ListItem button>
-              <KeyboardIcon sx={{ marginRight: 2 }} />
-              <ListItemText primary="Input" />
-            </ListItem>
-          </a>
-          <a href="#movement">
-            <ListItem button>
-              <DoubleArrowIcon sx={{ marginRight: 2 }} />
-              <ListItemText primary="Movement" />
-            </ListItem>
-          </a>
-          <a href="#tiles">
-            <ListItem button>
-              <GridOnIcon sx={{ marginRight: 2 }} />
-              <ListItemText primary="Tiles" />
-            </ListItem>
-          </a>
-        </List>
-      </Drawer>
-      <div className={styles.content}>
-        <h1><DescriptionIcon fontSize="large" />Docs</h1>
-        These docs provide an overview of the Codetrain engine functionality.
-        <hr />
-        <div id="gameloop" className={styles.bookmark} />
-        <h2>
-          <LoopIcon />
-          Game Loop
-          <a href="#gameloop">
-            <LinkIcon />
-          </a>
-        </h2>
-        There are two built-in game loop functions, <code>start()</code> and
-        {' '}<code>update()</code>.<br /><br />
-        <code>start()</code> runs once at the start of the game.<br />
-        <code>update()</code> runs once a frame.
-        <Code>
-{`let frames = 0;
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: 256,
+              boxSizing: 'border-box',
+              top: 60,
+              zIndex: 0
+            }
+          }}
+          variant="permanent"
+          anchor="left"
+        >
+          <List>
+            <a href="#gameloop">
+              <ListItem button>
+                <LoopIcon sx={{ marginRight: 2 }} />
+                <ListItemText primary="Game Loop" />
+              </ListItem>
+            </a>
+            <a href="#output">
+              <ListItem button>
+                <TextsmsIcon sx={{ marginRight: 2 }} />
+                <ListItemText primary="Output" />
+              </ListItem>
+            </a>
+            <a href="#input">
+              <ListItem button>
+                <KeyboardIcon sx={{ marginRight: 2 }} />
+                <ListItemText primary="Input" />
+              </ListItem>
+            </a>
+            <a href="#movement">
+              <ListItem button>
+                <DoubleArrowIcon sx={{ marginRight: 2 }} />
+                <ListItemText primary="Movement" />
+              </ListItem>
+            </a>
+            <a href="#tiles">
+              <ListItem button>
+                <GridOnIcon sx={{ marginRight: 2 }} />
+                <ListItemText primary="Tiles" />
+              </ListItem>
+            </a>
+          </List>
+        </Drawer>
+        <div className={styles.content}>
+          <h1><DescriptionIcon fontSize="large" />Docs</h1>
+          These docs provide an overview of the Codetrain engine functionality.
+          <hr />
+          <div id="gameloop" className={styles.bookmark} />
+          <h2>
+            <LoopIcon />
+            Game Loop
+            <a href="#gameloop">
+              <LinkIcon />
+            </a>
+          </h2>
+          There are two built-in game loop functions, <code>start()</code> and
+          {' '}<code>update()</code>.<br /><br />
+          <code>start()</code> runs once at the start of the game.<br />
+          <code>update()</code> runs once a frame.
+          <Code>
+  {`let frames = 0;
 
-function start() {
-  console.log(frames); // runs before update, logs 0
-}
+  function start() {
+    console.log(frames); // runs before update, logs 0
+  }
 
-function update() {
-  frames += 1;
-}`}
-        </Code>
-        <hr />
-        <div id="output" className={styles.bookmark} />
-        <h2>
-          <TextsmsIcon />
-          Output
-          <a href="#output">
-            <LinkIcon />
-          </a>
-        </h2>
-        A dialogue box can be opened with <code>say(text)</code>.<br />
-        <Code>
-{`say('Hello World!');`}
-        </Code>
-        <hr />
-        <div id="input" className={styles.bookmark} />
-        <h2>
-          <KeyboardIcon />
-          Input
-          <a href="#input">
-            <LinkIcon />
-          </a>
-        </h2>
-        Keyboard input can be taken with <code>isKeyDown(key)</code> and{' '}
-        <code>isKey(key)</code>.<br /><br />
-        <code>isKeyDown(key)</code> returns whether given <code>key</code> is
-        {' '}down.<br />
-        <code>isKey(key)</code> returns whether given <code>key</code> was
-        {' '}pressed in the last frame.
-        <Code>
-{`function update() {
-  if (isKey('q')) say('Q pressed'); // triggers once
-  if (isKeyDown('q')) console.log('Q down'); // triggers continually
-}
-`}
-        </Code>
-        <hr />
-        <div id="movement" className={styles.bookmark} />
-        <h2>
-          <DoubleArrowIcon />
-          Movement
-          <a href="#movement">
-            <LinkIcon />
-          </a>
-        </h2>
-        Objects can be moved with <code>move(dir)</code>,
-        {' '}<code>moveTiles(x, y)</code>, and <code>movePixels(x, y)</code>.
-        <br /><br />
-        <code>move(dir)</code> moves object one tile <code>up</code>,{' '}
-        <code>down</code>, <code>left</code>, or <code>right</code>.<br />
-        <code>moveTiles(x, y)</code> moves object by <code>x</code>,{' '}
-        <code>y</code>, in tiles.<br />
-        <code>movePixels(x, y)</code> moves object by <code>x</code>,{' '}
-        <code>y</code>, in pixels.
-        <Code>
-{`// tiled object movement
-function update() {
-  if (isKey('w')) move('up');
-  if (isKey('a')) move('left');
-  if (isKey('s')) move('down');
-  if (isKey('d')) move('right');
-}
-`}
-        </Code>
-        <Code>
-{`// smooth object movement
-function update() {
-  if (isKeyDown('w')) movePixels(0, -1);
-  if (isKeyDown('a')) movePixels(-1, 0);
-  if (isKeyDown('s')) movePixels(0, 1);
-  if (isKeyDown('d')) movePixels(1, 0);
-}`}
-        </Code>
-        Object position can be found with <code>getTilePosition()</code> and
-        {' '}<code>getPixelPosition()</code>.<br /><br />
-        <code>getTilePosition()</code> returns <code>x</code>, <code>y</code>
-        {' '}position in tiles.<br />
-        <code>getPixelPosition()</code> returns <code>x</code>, <code>y</code>
-        {' '}position in pixels.
-        <Code>
-{`const pos = getTilePosition();
+  function update() {
+    frames += 1;
+  }`}
+          </Code>
+          <hr />
+          <div id="output" className={styles.bookmark} />
+          <h2>
+            <TextsmsIcon />
+            Output
+            <a href="#output">
+              <LinkIcon />
+            </a>
+          </h2>
+          A dialogue box can be opened with <code>say(text)</code>.<br />
+          <Code>
+  {`say('Hello World!');`}
+          </Code>
+          <hr />
+          <div id="input" className={styles.bookmark} />
+          <h2>
+            <KeyboardIcon />
+            Input
+            <a href="#input">
+              <LinkIcon />
+            </a>
+          </h2>
+          Keyboard input can be taken with <code>isKeyDown(key)</code> and{' '}
+          <code>isKey(key)</code>.<br /><br />
+          <code>isKeyDown(key)</code> returns whether given <code>key</code> is
+          {' '}down.<br />
+          <code>isKey(key)</code> returns whether given <code>key</code> was
+          {' '}pressed in the last frame.
+          <Code>
+  {`function update() {
+    if (isKey('q')) say('Q pressed'); // triggers once
+    if (isKeyDown('q')) console.log('Q down'); // triggers continually
+  }
+  `}
+          </Code>
+          <hr />
+          <div id="movement" className={styles.bookmark} />
+          <h2>
+            <DoubleArrowIcon />
+            Movement
+            <a href="#movement">
+              <LinkIcon />
+            </a>
+          </h2>
+          Objects can be moved with <code>move(dir)</code>,
+          {' '}<code>moveTiles(x, y)</code>, and <code>movePixels(x, y)</code>.
+          <br /><br />
+          <code>move(dir)</code> moves object one tile <code>up</code>,{' '}
+          <code>down</code>, <code>left</code>, or <code>right</code>.<br />
+          <code>moveTiles(x, y)</code> moves object by <code>x</code>,{' '}
+          <code>y</code>, in tiles.<br />
+          <code>movePixels(x, y)</code> moves object by <code>x</code>,{' '}
+          <code>y</code>, in pixels.
+          <Code>
+  {`// tiled object movement
+  function update() {
+    if (isKey('w')) move('up');
+    if (isKey('a')) move('left');
+    if (isKey('s')) move('down');
+    if (isKey('d')) move('right');
+  }
+  `}
+          </Code>
+          <Code>
+  {`// smooth object movement
+  function update() {
+    if (isKeyDown('w')) movePixels(0, -1);
+    if (isKeyDown('a')) movePixels(-1, 0);
+    if (isKeyDown('s')) movePixels(0, 1);
+    if (isKeyDown('d')) movePixels(1, 0);
+  }`}
+          </Code>
+          Object position can be found with <code>getTilePosition()</code> and
+          {' '}<code>getPixelPosition()</code>.<br /><br />
+          <code>getTilePosition()</code> returns <code>x</code>, <code>y</code>
+          {' '}position in tiles.<br />
+          <code>getPixelPosition()</code> returns <code>x</code>, <code>y</code>
+          {' '}position in pixels.
+          <Code>
+  {`const pos = getTilePosition();
 
-say(\`Object is at \${x}, \${y}\`);`}
-        </Code>
-        <hr />
-        <div id="tiles" className={styles.bookmark} />
-        <h2>
-          <GridOnIcon />
-          Tiles
-          <a href="#tiles">
-            <LinkIcon />
-          </a>
-        </h2>
-        Tiles can be retrieved with <code>getTile()</code> and
-        {' '}<code>getTileAt(x, y, tile)</code>.<br /><br />
-        <code>getTile()</code> returns index of tile at current position.<br />
-        <code>getTileAt(x, y)</code> returns index of tile at <code>x</code>,
-        {' '}<code>y</code>.
-        <Code>
-{`const tile = getTile();
+  say(\`Object is at \${x}, \${y}\`);`}
+          </Code>
+          <hr />
+          <div id="tiles" className={styles.bookmark} />
+          <h2>
+            <GridOnIcon />
+            Tiles
+            <a href="#tiles">
+              <LinkIcon />
+            </a>
+          </h2>
+          Tiles can be retrieved with <code>getTile()</code> and
+          {' '}<code>getTileAt(x, y, tile)</code>.<br /><br />
+          <code>getTile()</code> returns index of tile at current position.<br />
+          <code>getTileAt(x, y)</code> returns index of tile at <code>x</code>,
+          {' '}<code>y</code>.
+          <Code>
+  {`const tile = getTile();
 
-say(\`Object is on tile \${tile}\`);
-`}
-        </Code>
-        Tiles can be modified with <code>setTile(tile)</code> and
-        {' '}<code>setTileAt(x, y, tile)</code>.<br /><br />
-        <code>setTile(tile)</code> returns index of tile at current position.<br />
-        <code>setTileAt(x, y, tile)</code> sets tile at <code>x</code>,
-        {' '}<code>y</code> to index <code>tile</code>.
-        <Code>
-{`const tile = getTile();
+  say(\`Object is on tile \${tile}\`);
+  `}
+          </Code>
+          Tiles can be modified with <code>setTile(tile)</code> and
+          {' '}<code>setTileAt(x, y, tile)</code>.<br /><br />
+          <code>setTile(tile)</code> returns index of tile at current position.<br />
+          <code>setTileAt(x, y, tile)</code> sets tile at <code>x</code>,
+          {' '}<code>y</code> to index <code>tile</code>.
+          <Code>
+  {`const tile = getTile();
 
-// toggle tile at position
-if (tile === 0) setTile(1);
-else setTile(0);
-`}
-        </Code>
+  // toggle tile at position
+  if (tile === 0) setTile(1);
+  else setTile(0);
+  `}
+          </Code>
+        </div>
       </div>
     </div>
   )
