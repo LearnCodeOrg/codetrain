93582ff5f88fc80f5cb7e63a9901cbbfefe4ba8e
Cooper Saye <coopersaye@gmail.com>
2021-10-14 20:56:10 -0700

    Fix challenge card links.

--- components/cards/Challenge.js
+++ components/cards/Challenge.js
@@ -1,14 +1,18 @@
+import Link from 'next/link';
+
 import styles from '../../styles/components/cards/Challenge.module.css';
 
 export default function Challenge(props) {
   const { id, title, description } = props;
 
   return (
-    <a href={`/challenges/${id}`} className={styles.container}> {/* eslint-disable-line @next/next/no-html-link-for-pages */}
-      <div>
-        <h1>{title}</h1>
-        <p>{description}</p>
-      </div>
-    </a>
+    <Link href={`/challenges/${id}`}>
+      <a className={styles.container}>
+        <div>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </div>
+      </a>
+    </Link>
   );
 }
