172aaa5cf9e4d3e71e7241dfbca7eb2781f8cdd4
Cooper Saye <coopersaye@gmail.com>
2021-10-14 22:28:02 -0700

    Restructure header.

--- components/Header.js
+++ components/Header.js
@@ -19,38 +19,35 @@ export default function Header(props) {
   const { username } = props;
 
   return (
-    <>
-      <div className={styles.container}>
-        <a href="/" className={styles.icon}> {/* eslint-disable-line @next/next/no-html-link-for-pages */}
-          <Image src="/logo.png" height="48" width="48" alt="logo" />
-        </a>
-        <h1>Codetrain</h1>
-        <span className="flexfill" />
-        <a href="/" className={styles.link}>Home</a> {/* eslint-disable-line @next/next/no-html-link-for-pages */}
-        <a href="/create" className={styles.link}>Create</a> {/* eslint-disable-line @next/next/no-html-link-for-pages */}
-        <a href="/explore" className={styles.link}>Explore</a> {/* eslint-disable-line @next/next/no-html-link-for-pages */}
-        <a href="/docs" className={styles.link}>Docs</a> {/* eslint-disable-line @next/next/no-html-link-for-pages */}
-        {
-          username === null ?
-          <Tooltip title="Choose Username" arrow>
-            <IconButton onClick={createUser}>
-              <HelpOutlineIcon />
-            </IconButton>
-          </Tooltip> :
-          username ?
-          <Tooltip title="Sign Out" arrow>
-            <IconButton onClick={() => firebase.auth().signOut()}>
-              <ExitToAppIcon />
-            </IconButton>
-          </Tooltip> :
-          <Tooltip title="Sign In" arrow>
-            <IconButton onClick={signInWithGoogle}>
-              <PersonOutlineIcon />
-            </IconButton>
-          </Tooltip>
-        }
-      </div>
-      <div style={{ height: 60 }} />
-    </>
+    <div className={styles.container}>
+      <a href="/" className={styles.icon}> {/* eslint-disable-line @next/next/no-html-link-for-pages */}
+        <Image src="/logo.png" height="48" width="48" alt="logo" />
+      </a>
+      <h1>Codetrain</h1>
+      <span className="flexfill" />
+      <a href="/" className={styles.link}>Home</a> {/* eslint-disable-line @next/next/no-html-link-for-pages */}
+      <a href="/create" className={styles.link}>Create</a> {/* eslint-disable-line @next/next/no-html-link-for-pages */}
+      <a href="/explore" className={styles.link}>Explore</a> {/* eslint-disable-line @next/next/no-html-link-for-pages */}
+      <a href="/docs" className={styles.link}>Docs</a> {/* eslint-disable-line @next/next/no-html-link-for-pages */}
+      {
+        username === null ?
+        <Tooltip title="Choose Username" arrow>
+          <IconButton onClick={createUser}>
+            <HelpOutlineIcon />
+          </IconButton>
+        </Tooltip> :
+        username ?
+        <Tooltip title="Sign Out" arrow>
+          <IconButton onClick={() => firebase.auth().signOut()}>
+            <ExitToAppIcon />
+          </IconButton>
+        </Tooltip> :
+        <Tooltip title="Sign In" arrow>
+          <IconButton onClick={signInWithGoogle}>
+            <PersonOutlineIcon />
+          </IconButton>
+        </Tooltip>
+      }
+    </div>
   );
 }
