cc39b2eec0a046b213fba0bef4157940580b907c
Cooper Saye <coopersaye@gmail.com>
2021-10-17 22:48:01 -0700

    Improve homepage layout.

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,9 @@ export default function Index() {
       <Header />
       <div className={styles.center}>
         <WaveDivider height="60px" color="var(--main2)" />
+        <div className={styles.logo}>
+          <Image src="/logo.png" width="96" height="96" />
+        </div>
         <h2>Codetrain makes game development easier than ever.</h2>
         <p>No downloads, no dependencies: <b>create games right from your browser.</b><br />
         Easily export to HTML for portability. Explore and remix projects from
@@ -30,33 +33,36 @@ export default function Index() {
             <a>Docs</a>
           </Link>
         </div>
-        <div className={styles.accordion}>
-          <Accordion title="What is Codetrain?">
-            Codetrain is a retro game engine in the browser.
-          </Accordion>
-          <Accordion title="Who is Codetrain for?">
-            Everyone. Whether you&apos;re a beginner looking to learn more about
-            game development or an expert looking to challenge themselves in a
-            constrained environment, Codetrain provides a platform for you.
-          </Accordion>
-          <Accordion title="How can I get started?">
-            Check out our{' '}
-            <Link href="/docs">
-              <a>docs</a>
-            </Link>
-            {' '}or jump straight into the{' '}
-            <Link href="/create">
-              <a>engine</a>
-            </Link>.
-          </Accordion>
-          <Accordion title="Can I contribute?">
-            Codetrain is open source. Want to contribute? Find our GitHub
-            {' '}repository <a
-              href="https://github.com/codeconvoy/codetrain"
-              target="_blank"
-              rel="noopener noreferrer"
-            >here</a>.
-          </Accordion>
+        <div className={styles.bottom}>
+          <WaveDivider height="0" color="var(--main0)" />
+          <div className={styles.accordion}>
+            <Accordion title="What is Codetrain?">
+              Codetrain is a retro game engine in the browser.
+            </Accordion>
+            <Accordion title="Who is Codetrain for?">
+              Everyone. Whether you&apos;re a beginner looking to learn more about
+              game development or an expert looking to challenge themselves in a
+              constrained environment, Codetrain provides a platform for you.
+            </Accordion>
+            <Accordion title="How can I get started?">
+              Check out our{' '}
+              <Link href="/docs">
+                <a>docs</a>
+              </Link>
+              {' '}or jump straight into the{' '}
+              <Link href="/create">
+                <a>engine</a>
+              </Link>.
+            </Accordion>
+            <Accordion title="Can I contribute?">
+              Codetrain is open source. Want to contribute? Find our GitHub
+              {' '}repository <a
+                href="https://github.com/codeconvoy/codetrain"
+                target="_blank"
+                rel="noopener noreferrer"
+              >here</a>.
+            </Accordion>
+          </div>
         </div>
       </div>
       <div className="flexfill" />
