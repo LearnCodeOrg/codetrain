441b19339cf58216dfce04537da058c9fadf92a3
Cooper Saye <coopersaye@gmail.com>
2021-10-20 11:29:36 -0700

    Implement setup page routing.

--- pages/setup.js
+++ pages/setup.js
@@ -1,6 +1,21 @@
+import Loading from '../components/Loading';
+import Router from 'next/router';
+
+import { useEffect, useState } from 'react';
+
 import styles from '../styles/pages/Setup.module.css';
 
-export default function Setup() {
+export default function Setup(props) {
+  const { username } = props;
+
+  // listen for auth
+  useEffect(() => {
+    if (username === undefined) Router.push('/');
+  }, [username]);
+
+  // return if loading
+  if (username !== null) return <Loading />;
+
   return (
     <div>
     </div>
