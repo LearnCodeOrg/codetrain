a2a2140fa9637e302bda7127012ecb25a186a9c5
Cooper Saye <coopersaye@gmail.com>
2021-10-23 22:08:35 -0700

    Create default player.

--- data/engine.js
+++ data/engine.js
@@ -18,16 +18,47 @@ function update() {
 
 }
 `;
+const playerCode =
+`// called once when the game starts
+function start() {
+
+}
+
+// called every frame
+function update() {
+  if (isKey('w')) move('up');
+  if (isKey('a')) move('left');
+  if (isKey('s')) move('down');
+  if (isKey('d')) move('right');
+}
+`;
+const playerObject = [
+  0, 0, 2, 2, 2, 0, 2, 0,
+  0, 2, 2, 2, 2, 0, 2, 0,
+  0, 1, 3, 1, 3, 0, 2, 0,
+  0, 1, 1, 1, 1, 0, 2, 0,
+  2, 2, 2, 2, 2, 3, 3, 3,
+  1, 2, 2, 2, 2, 1, 1, 0,
+  1, 2, 2, 2, 2, 0, 2, 0,
+  0, 3, 0, 0, 3, 0, 0, 0
+];
 const defaultSprite = Array(spriteSize * spriteSize).fill(0);
-const codes = Array(objectCount).fill(defaultCode);
-const objectNames = Array(objectCount).fill(0).map((val, i) => `Object ${i}`);
-const colors = palettes[0].colors;
-const tiles = Array(tileCount).fill(defaultSprite);
-const objects = Array(objectCount).fill(defaultSprite);
-const background = Array(mapSize * mapSize).fill(0);
+export const codes = Array(objectCount).fill(0).map((val, i) => (
+  i === 0 ? playerCode : defaultCode
+));
+export const objectNames = Array(objectCount).fill(0).map((val, i) => (
+  i === 0 ? 'Player' : `Object ${i}`
+));
+export const colors = palettes[0].colors;
+export const tiles = Array(tileCount).fill(defaultSprite);
+export const objects = Array(objectCount).fill(0).map((val, i) => (
+  i === 0 ? playerObject : defaultSprite
+));
+export const background = Array(mapSize * mapSize).fill(0);
 
 // default data
 export const defaultData = {
-  codes, colors, background, gameObjects: [], title: '', description: '',
-  objectNames, tiles: JSON.stringify(tiles), objects: JSON.stringify(objects)
+  codes, colors, background, gameObjects: [{ sprite: 0, x: 0, y: 0 }],
+  title: 'My First Project', description: 'WASD to move.', objectNames,
+  tiles: JSON.stringify(tiles), objects: JSON.stringify(objects)
 };
