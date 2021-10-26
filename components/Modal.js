22828e3f68b6f4cefdc233b3bc03af83195eb9d0
Cooper Saye <coopersaye@gmail.com>
2021-10-25 23:01:19 -0700

    Implement modal transition.

--- components/Modal.js
+++ components/Modal.js
@@ -1,4 +1,6 @@
 import BaseModal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Backdrop from '@mui/material/Backdrop';
 import ClearIcon from '@mui/icons-material/Clear';
 
 import styles from '../styles/components/Modal.module.css';
@@ -12,13 +14,24 @@ export default function Modal(props) {
   }
 
   return (
-    <BaseModal open={open} onClose={onClose}>
-      <div>
-        <button onClick={onClose}>
-          <ClearIcon />
-        </button>
-        {props.children}
-      </div>
+    <BaseModal
+      open={open}
+      onClose={onClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        className: styles.backdrop,
+        timeout: 100
+      }}
+    >
+      <Fade in={open} timeout={100}>
+        <div>
+          <button onClick={onClose}>
+            <ClearIcon />
+          </button>
+          {props.children}
+        </div>
+      </Fade>
     </BaseModal>
   );
 }
