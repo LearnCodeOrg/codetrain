6963a5cedf3e6a12a5bb8a39bbbaee53ab09a9f3
Cooper Saye <coopersaye@gmail.com>
2021-11-04 23:25:16 -0700

    Update site meta.

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,7 @@ import '../styles/globals.css';
 
 // meta
 const title = "Codetrain";
+const url = "https://codetrain.org";
 const description = "A retro game engine in the browser.";
 const image = "https://codetrain.org/img/cover.png";
 
@@ -23,24 +24,24 @@ export default function App(props) {
   return (
     <>
       <Head>
-        <title>Codetrain</title>
+        <title>{title}</title>
         <meta name="description" content={description} />
         {/* links */}
         <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <link href="https://codetrain.org" rel="canonical" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;900&display=swap" rel="stylesheet" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="canonical" href={url} />
         {/* open graph */}
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://codetrain.org" />
+        <meta property="og:url" content={url} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={image} />
         {/* twitter */}
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@csaye_" />
+        <meta name="twitter:site" content="@CodeConvoyOrg" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={image} />
