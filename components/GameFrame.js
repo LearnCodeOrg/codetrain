9d1bbda9e2445207c24138aee04051eb97139c0e
Cooper Saye <coopersaye@gmail.com>
2021-11-17 18:57:55 -0800

    Abstract game variable scoping.

--- components/GameFrame.js
+++ components/GameFrame.js
@@ -30,9 +30,9 @@ export default function GameFrame(props) {
   const getTilePos = () => __getTilePos__(index);
   const getTile = () => __getTile__(index);
   const setTile = tile => __setTile__(index, tile);
-  const getTileAt = (x, y) => background[y * mapSize + x];
-  const setTileAt = (x, y, tile) => { background[y * mapSize + x] = tile; }
-  const say = text => { dialogue = \`\${text}\`; }
+  const getTileAt = (x, y) => $$.background[y * $$.mapSize + x];
+  const setTileAt = (x, y, tile) => { $$.background[y * $$.mapSize + x] = tile; }
+  const say = text => { $$.dialogue = \`\${text}\`; }
   ${codes[sprite]}
   return {
     start: typeof start === 'function' ? start : () => {},
@@ -59,84 +59,86 @@ export default function GameFrame(props) {
   </style>
   <script>
     // variable declarations
-    let dialogue;
-    const mapSize = ${mapSize};
-    const mapPixels = ${mapPixels};
-    const spriteSize = ${spriteSize};
-    const spritePixels = ${spritePixels};
-    const pixelPixels = ${pixelPixels};
-    const colors = ${JSON.stringify(colors)};
-    const tiles = ${JSON.stringify(tiles)};
-    const objects = ${JSON.stringify(objects)};
-    const background = ${JSON.stringify(background)};
-    const gameObjects = ${JSON.stringify(gameObjects)};
-    let lastPressedKeys = {};
-    const pressedKeys = {};
+    const $$ = {
+      dialogue: undefined,
+      mapSize: ${mapSize},
+      mapPixels: ${mapPixels},
+      spriteSize: ${spriteSize},
+      spritePixels: ${spritePixels},
+      pixelPixels: ${pixelPixels},
+      colors: ${JSON.stringify(colors)},
+      tiles: ${JSON.stringify(tiles)},
+      objects: ${JSON.stringify(objects)},
+      background: ${JSON.stringify(background)},
+      gameObjects: ${JSON.stringify(gameObjects)},
+      lastPressedKeys: {},
+      pressedKeys: {}
+    }
     // sprite functions
     function __move__(index, dir) {
-      if (dir === 'up') gameObjects[index].y -= spritePixels;
-      else if (dir === 'down') gameObjects[index].y += spritePixels;
-      else if (dir === 'left') gameObjects[index].x -= spritePixels;
-      else if (dir === 'right') gameObjects[index].x += spritePixels;
+      if (dir === 'up') $$.gameObjects[index].y -= $$.spritePixels;
+      else if (dir === 'down') $$.gameObjects[index].y += $$.spritePixels;
+      else if (dir === 'left') $$.gameObjects[index].x -= $$.spritePixels;
+      else if (dir === 'right') $$.gameObjects[index].x += $$.spritePixels;
     }
     function __movePixels__(index, x, y) {
-      gameObjects[index].x += x * pixelPixels;
-      gameObjects[index].y += y * pixelPixels;
+      $$.gameObjects[index].x += x * $$.pixelPixels;
+      $$.gameObjects[index].y += y * $$.pixelPixels;
     }
     function __moveTiles__(index, x, y) {
-      gameObjects[index].x += x * spritePixels;
-      gameObjects[index].y += y * spritePixels;
+      $$.gameObjects[index].x += x * $$.spritePixels;
+      $$.gameObjects[index].y += y * $$.spritePixels;
     }
     function __setPixelPos__(index, x, y) {
-      gameObjects[index].x = x * pixelPixels;
-      gameObjects[index].y = y * pixelPixels;
+      $$.gameObjects[index].x = x * $$.pixelPixels;
+      $$.gameObjects[index].y = y * $$.pixelPixels;
     }
     function __setTilePos__(index, x, y) {
-      gameObjects[index].x = x * spritePixels;
-      gameObjects[index].y = y * spritePixels;
+      $$.gameObjects[index].x = x * $$.spritePixels;
+      $$.gameObjects[index].y = y * $$.spritePixels;
     }
     function __getPixelPos__(index) {
       return {
-        x: Math.floor(gameObjects[index].x / pixelPixels),
-        y: Math.floor(gameObjects[index].y / pixelPixels)
+        x: Math.floor($$.gameObjects[index].x / $$.pixelPixels),
+        y: Math.floor($$.gameObjects[index].y / $$.pixelPixels)
       };
     }
     function __getTilePos__(index) {
       return {
-        x: Math.floor(gameObjects[index].x / spritePixels),
-        y: Math.floor(gameObjects[index].y / spritePixels)
+        x: Math.floor($$.gameObjects[index].x / $$.spritePixels),
+        y: Math.floor($$.gameObjects[index].y / $$.spritePixels)
       };
     }
     function __getTile__(index) {
       const position = __getTilePosition__(index);
-      const tileIndex = position.y * mapSize + position.x;
-      return background[tileIndex];
+      const tileIndex = position.y * $$.mapSize + position.x;
+      return $$.background[tileIndex];
     }
     function __setTile__(index, tile) {
       const position = __getTilePosition__(index);
-      const tileIndex = position.y * mapSize + position.x;
-      background[tileIndex] = tile;
+      const tileIndex = position.y * $$.mapSize + position.x;
+      $$.background[tileIndex] = tile;
     }
     // set up input listeners
     window.onkeydown = e => {
-      pressedKeys[e.keyCode] = true;
-      dialogue = undefined;
+      $$.pressedKeys[e.keyCode] = true;
+      $$.dialogue = undefined;
     }
-    window.onkeyup = e => pressedKeys[e.keyCode] = false;
-    window.onmousedown = e => { dialogue = undefined; }
+    window.onkeyup = e => $$.pressedKeys[e.keyCode] = false;
+    window.onmousedown = e => { $$.dialogue = undefined; }
     function isKeyDown(key) {
       // handle invalid key
       if (typeof key !== 'string' || !key.length) return undefined;
       // handle key code
       const keyCode = key.toUpperCase().charCodeAt(0);
-      return pressedKeys[keyCode];
+      return $$.pressedKeys[keyCode];
     }
     function isKey(key) {
       // handle invalid key
       if (typeof key !== 'string' || !key.length) return undefined;
       // handle key code
       const keyCode = key.toUpperCase().charCodeAt(0);
-      return pressedKeys[keyCode] && !lastPressedKeys[keyCode];
+      return $$.pressedKeys[keyCode] && !$$.lastPressedKeys[keyCode];
     }
     // sprite codes
     const spriteCodes = [
@@ -151,55 +153,55 @@ export default function GameFrame(props) {
       // canvas functions
       function drawSprite(sprite, x, y) {
         // for each pixel
-        for (let yp = 0; yp < spriteSize; yp++) {
-          for (let xp = 0; xp < spriteSize; xp++) {
+        for (let yp = 0; yp < $$.spriteSize; yp++) {
+          for (let xp = 0; xp < $$.spriteSize; xp++) {
             // set fill color
-            const colorIndex = yp * spriteSize + xp;
-            const color = colors[sprite[colorIndex]];
+            const colorIndex = yp * $$.spriteSize + xp;
+            const color = $$.colors[sprite[colorIndex]];
             ctx.fillStyle = color;
             // get fill position
-            let xm = x + xp * pixelPixels;
-            let ym = y + yp * pixelPixels;
+            let xm = x + xp * $$.pixelPixels;
+            let ym = y + yp * $$.pixelPixels;
             // fill pixel
-            ctx.fillRect(xm, ym, pixelPixels, pixelPixels);
+            ctx.fillRect(xm, ym, $$.pixelPixels, $$.pixelPixels);
           }
         }
       }
       function draw() {
         // for each tile
-        for (let y = 0; y < mapSize; y++) {
-          for (let x = 0; x < mapSize; x++) {
+        for (let y = 0; y < $$.mapSize; y++) {
+          for (let x = 0; x < $$.mapSize; x++) {
             // get sprite
-            const spriteIndex = y * mapSize + x;
-            const sprite = tiles[background[spriteIndex]];
+            const spriteIndex = y * $$.mapSize + x;
+            const sprite = $$.tiles[$$.background[spriteIndex]];
             // draw sprite
-            drawSprite(sprite, x * spritePixels, y * spritePixels);
+            drawSprite(sprite, x * $$.spritePixels, y * $$.spritePixels);
           }
         }
         // for each object
-        for (const object of gameObjects) {
+        for (const object of $$.gameObjects) {
           // draw object
           const { x, y } = object;
-          const sprite = objects[object.sprite];
+          const sprite = $$.objects[object.sprite];
           drawSprite(sprite, x, y);
         }
         // draw dialogue text
-        if (dialogue) {
-          const left = mapPixels / 8;
-          const top = mapPixels * 3 / 8;
+        if ($$.dialogue) {
+          const left = $$.mapPixels / 8;
+          const top = $$.mapPixels * 3 / 8;
           const offset = 8;
-          const width = mapPixels * 3 / 4 + offset * 2;
-          const height = mapPixels / 4 + offset * 2;
+          const width = $$.mapPixels * 3 / 4 + offset * 2;
+          const height = $$.mapPixels / 4 + offset * 2;
           ctx.fillStyle = '#fff';
           ctx.fillRect(left - offset, top - offset, width, height);
           ctx.fillStyle = '#000';
           const fontSize = 16;
           const lineSize = 20;
           ctx.font = \`\${fontSize}px monospace\`;
-          const line1 = dialogue.slice(0, lineSize);
-          const line2 = dialogue.slice(lineSize, lineSize * 2);
-          const line3 = dialogue.slice(lineSize * 2, lineSize * 3);
-          const line4 = dialogue.slice(lineSize * 3, lineSize * 4);
+          const line1 = $$.dialogue.slice(0, lineSize);
+          const line2 = $$.dialogue.slice(lineSize, lineSize * 2);
+          const line3 = $$.dialogue.slice(lineSize * 2, lineSize * 3);
+          const line4 = $$.dialogue.slice(lineSize * 3, lineSize * 4);
           ctx.fillText(line1, left, top + fontSize);
           ctx.fillText(line2, left, top + fontSize * 2);
           ctx.fillText(line3, left, top + fontSize * 3);
@@ -213,7 +215,7 @@ export default function GameFrame(props) {
         // draw
         draw();
         // update keys
-        lastPressedKeys = Object.assign({}, pressedKeys);
+        $$.lastPressedKeys = Object.assign({}, $$.pressedKeys);
         // continue loop
         requestAnimationFrame(gameLoop);
       }
