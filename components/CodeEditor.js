d935437bcbcaca44d65be07265a32bcb1de3ce10
Cooper Saye <coopersaye@gmail.com>
2021-11-19 22:52:12 -0800

    Enable code autocompletion.

--- components/CodeEditor.js
+++ components/CodeEditor.js
@@ -2,6 +2,7 @@ import AceEditor from 'react-ace';
 
 import 'ace-builds/src-noconflict/mode-javascript.js';
 import 'ace-builds/src-noconflict/theme-monokai.js';
+import 'ace-builds/src-noconflict/ext-language_tools';
 
 export default function CodeEditor(props) {
   return (
@@ -11,7 +12,10 @@ export default function CodeEditor(props) {
       wrapEnabled={true}
       showPrintMargin={false}
       tabSize={2}
-      setOptions={{ useWorker: false }}
+      setOptions={{
+        useWorker: false,
+        enableLiveAutocompletion: true
+      }}
       width="500px"
       height="100%"
       {...props}
