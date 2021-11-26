3314b9dd99c60b4c60b6fc79ff085fe4291206ed
Cooper Saye <coopersaye@gmail.com>
2021-11-26 12:14:19 -0800

    Implement palette starring.

--- components/cards/Palette.js
+++ components/cards/Palette.js
@@ -9,10 +9,28 @@ import styles from '../../styles/components/cards/Palette.module.css';
 export default function Palette(props) {
   const { name, colors, created, uid, id, userPalettes } = props;
 
+  const currentUser = firebase.auth().currentUser;
+  const usersRef = firebase.firestore().collection('users');
+
+  const palettesRef = firebase.firestore().collection('palettes');
+  const paletteRef = palettesRef.doc(id);
+
   // deletes palette from firebase
   async function deletePalette() {
     if (!window.confirm(`Delete palette "${name}"?`)) return;
-    await firebase.firestore().collection('palettes').doc(id).delete();
+    await paletteRef.delete();
+  }
+
+  // sets user star status in firebase
+  async function setStar(doStar) {
+    const palette = { name, colors, id };
+    const userRef = usersRef.doc(currentUser.uid);
+    await userRef.update({
+      palettes:
+        doStar ?
+        firebase.firestore.FieldValue.arrayUnion(palette) :
+        firebase.firestore.FieldValue.arrayRemove(palette)
+    });
   }
 
   return (
