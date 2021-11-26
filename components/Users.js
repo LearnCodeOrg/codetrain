b42f1e0b877b8548f7de7187e39ed67b9d5f95c8
Cooper Saye <coopersaye@gmail.com>
2021-11-25 19:18:17 -0800

    Create Users component.

---
+++ components/Users.js
@@ -0,0 +1,37 @@
+import User from './cards/User';
+import Loading from './Loading';
+
+import { useEffect, useState } from 'react';
+import firebase from 'firebase/app';
+
+import styles from '../styles/components/Users.module.css';
+
+export default function Users() {
+  const [users, setUsers] = useState(undefined);
+
+  // retrieves users from firebase
+  async function getUsers() {
+    const usersRef = firebase.firestore().collection('users');
+    const usersQuery = usersRef.orderBy('joined', 'desc');
+    const usersDocs = (await usersQuery.get()).docs;
+    setUsers(usersDocs.map(doc => ({ id: doc.id, ...doc.data() })));
+  }
+
+  // get users on start
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  // return if loading
+  if (!users) return <Loading />;
+
+  return (
+    <div className={styles.container}>
+      {
+        users.map(user =>
+          <User {...user} key={user.id} />
+        )
+      }
+    </div>
+  );
+}
