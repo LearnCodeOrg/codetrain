1d2a76c35cdf89864999b4cae707bdb587ca46e6
Cooper Saye <coopersaye@gmail.com>
2021-11-25 19:03:42 -0800

    Extract Projects component.

---
+++ components/Projects.js
@@ -0,0 +1,38 @@
+import Project from './cards/Project';
+import Loading from './Loading';
+
+import { useEffect, useState } from 'react';
+import firebase from 'firebase/app';
+
+import styles from '../styles/components/Projects.module.css';
+
+export default function Projects() {
+  const [projects, setProjects] = useState(undefined);
+
+  // retrieves projects from firebase
+  async function getProjects() {
+    // get and set projects data
+    const projectsRef = firebase.firestore().collection('projects');
+    const projectsQuery = projectsRef.orderBy('modified', 'desc');
+    const projectsDocs = (await projectsQuery.get()).docs;
+    setProjects(projectsDocs.map(doc => ({ id: doc.id, ...doc.data() })));
+  }
+
+  // get projects on start
+  useEffect(() => {
+    getProjects();
+  }, []);
+
+  // return if loading
+  if (!projects) return <Loading />;
+
+  return (
+    <div className={styles.container}>
+      {
+        projects.map(project =>
+          <Project {...project} key={project.id} />
+        )
+      }
+    </div>
+  );
+}
