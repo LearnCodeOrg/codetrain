ec40641a65d4769d1ebd1a9ec2c7736edb98d4d0
Cooper Saye <coopersaye@gmail.com>
2021-11-26 22:47:23 -0800

    Load explore if undefined tab.

--- pages/explore.js
+++ pages/explore.js
@@ -1,4 +1,5 @@
 import Header from '../components/Header';
+import Loading from '../components/Loading';
 import Projects from '../components/Projects';
 import Users from '../components/Users';
 import Palettes from '../components/Palettes';
@@ -27,25 +28,31 @@ export default function Explore(props) {
     <div className={styles.container}>
       <Header {...props} />
       <h1><ExploreIcon fontSize="large" />Explore</h1>
-      <div className={styles.options}>
-        <select
-          value={tab}
-          onChange={e => {
-            const newTab = e.target.value;
-            setTab(newTab);
-            router.push(`/explore?tab=${newTab}`);
-          }}
-        >
-          <option value="projects">Projects</option>
-          <option value="users">Users</option>
-          <option value="palettes">Palettes</option>
-        </select>
-      </div>
-      <div className={styles.content}>
-        {(!tab || tab === 'projects') && <Projects />}
-        {tab === 'users' && <Users />}
-        {tab === 'palettes' && <Palettes userPalettes={userData?.palettes} />}
-      </div>
+      {
+        tab === undefined ?
+        <Loading /> :
+        <>
+          <div className={styles.options}>
+            <select
+              value={tab}
+              onChange={e => {
+                const newTab = e.target.value;
+                setTab(newTab);
+                router.push(`/explore?tab=${newTab}`);
+              }}
+            >
+              <option value="projects">Projects</option>
+              <option value="users">Users</option>
+              <option value="palettes">Palettes</option>
+            </select>
+          </div>
+          <div className={styles.content}>
+            {(!tab || tab === 'projects') && <Projects />}
+            {tab === 'users' && <Users />}
+            {tab === 'palettes' && <Palettes userPalettes={userData?.palettes} />}
+          </div>
+        </>
+      }
     </div>
   );
 }
