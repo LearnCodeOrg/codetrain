362b8c725b278cb1332e7cfb2558e973fd0bb2f2
Cooper Saye <coopersaye@gmail.com>
2021-11-26 17:58:26 -0800

    Create default palettes.

--- data/palettes.js
+++ data/palettes.js
@@ -8,38 +8,62 @@ export const defaultColors = [
 export const defaultPalettes = [
   {
     name: "Gameboy",
-    colors: ['#e0f8d0', '#88c070', '#346856', '#081820']
+    colors: [
+      "#e0f8d0",
+      "#88c070",
+      "#346856",
+      "#081820"
+    ],
+    id: "6UDQVS7KILUp5PrqVFTl"
   },
   {
     name: "Rustic",
-    colors: ['#edb4a1', '#a96868', '#764462', '#2c2137']
-  },
-  {
-    name: "Mist",
-    colors: ['#c4f0c2', '#5ab9a8', '#1e606e', '#2d1b00']
-  },
-  {
-    name: "Ice Cream",
-    colors: ['#fff6d3', '#f9a875', '#eb6b6f', '#7c3f58']
+    colors: [
+      "#edb4a1",
+      "#a96868",
+      "#764462",
+      "#2c2137"
+    ],
+    id: "9Gn9bcnJPZKXtsE3eEgt"
   },
   {
     name: "Kirokaze",
-    colors: ['#e2f3e4', '#94e344', '#46878f', '#332c50']
-  },
-  {
-    name: "Wish",
-    colors: ['#8be5ff', '#608fcf', '#7550e8', '#622e4c']
-  },
-  {
-    name: "Nostalgia",
-    colors: ['#d0d058', '#a0a840', '#708028', '#405010']
+    colors: [
+      "#e2f3e4",
+      "#94e344",
+      "#46878f",
+      "#332c50"
+    ],
+    id: "JOpFkB8DOjn6yFMQrpQc"
   },
   {
     name: "Nintendo",
-    colors: ['#f7e7c6', '#d68e49', '#a63725', '#331e50']
+    colors: [
+      "#f7e7c6",
+      "#d68e49",
+      "#a63725",
+      "#331e50"
+    ],
+    id: "VBqZjRxqTiZi3fJoNUnY"
   },
   {
-    name: "Heart",
-    colors: ['#ebe5ce', '#ff4589', '#a64777', '#3e2653']
+    name: "Wish",
+    colors: [
+      "#8be5ff",
+      "#608fcf",
+      "#7550e8",
+      "#622e4c"
+    ],
+    id: "g0KbkD3UJkymFDzUCgP3"
+  },
+  {
+    name: "Ice Cream",
+    colors: [
+      "#fff6d3",
+      "#f9a875",
+      "#eb6b6f",
+      "#7c3f58"
+    ],
+    id: "viX6YPqlb7tA6Ha5kbpd"
   }
 ];
