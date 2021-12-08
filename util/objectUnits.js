4b48f36f2975a92bd83307b635e0a57bbb134c68
Cooper Saye <coopersaye@gmail.com>
2021-12-07 22:05:54 -0800

    Fix object props in unit conversion.

--- util/objectUnits.js
+++ util/objectUnits.js
@@ -2,20 +2,26 @@ import { shortid } from './uuid';
 
 // returns given gameobjects with units inserted
 export function insertObjectUnits(gameObjects, pixelPixels) {
-  return gameObjects.map(obj => ({
-    id: obj.id ?? shortid(),
-    sprite: obj.sprite,
-    x: obj.x * pixelPixels,
-    y: obj.y * pixelPixels
-  }));
+  return gameObjects.map(obj => {
+    const { x, y, id, ...data } = obj;
+    return {
+      id: id ?? shortid(),
+      x: x * pixelPixels,
+      y: y * pixelPixels,
+      ...data
+    };
+  });
 }
 
 // returns given gameobjects with units removed
 export function removeObjectUnits(gameObjects, pixelPixels) {
-  return gameObjects.map(obj => ({
-    id: obj.id ?? shortid(),
-    sprite: obj.sprite,
-    x: Math.round(obj.x / pixelPixels),
-    y: Math.round(obj.y / pixelPixels)
-  }));
+  return gameObjects.map(obj => {
+    const { id, x, y, ...data } = obj;
+    return {
+      id: id ?? shortid(),
+      x: Math.round(x / pixelPixels),
+      y: Math.round(y / pixelPixels),
+      ...obj
+    };
+  });
 }
