a87d16c380c021f064ee989f6aa0c6a12d0f9d9c
Cooper Saye <coopersaye@gmail.com>
2021-12-09 22:48:19 -0800

    Extract UserPage content.

--- components/UserPage.js
+++ components/UserPage.js
@@ -2,7 +2,121 @@ import styles from '../styles/components/UserPage.module.css';
 
 export default function UserPage() {
   return (
-    <div>
+    <div className={styles.content}>
+      <div className={styles.head}>
+        <div className={styles.title}>
+          {
+            ownPage ?
+            <label>
+              <img src={userData.photo} />
+              <input
+                type="file"
+                accept="image/*"
+                onChange={e => updatePhoto(e.target.files[0])}
+                hidden={true}
+              />
+            </label> :
+            <img src={userData.photo} />
+          }
+          <div>
+            <h1>{userData.username}</h1>
+            <p>
+              Joined
+              {' '}
+              {
+                new Date(userData.joined)
+                .toLocaleDateString(undefined, { month: 'long' })
+              }
+              {' '}
+              {new Date(userData.joined).getFullYear()}
+            </p>
+          </div>
+        </div>
+        <div className={styles.description}>
+          {
+            editing ?
+            <input
+              placeholder="Description"
+              value={description}
+              onChange={e => setDescription(e.target.value)}
+              maxLength="2048"
+            /> :
+            <p>{description ?? userData.description}</p>
+          }
+          {
+            ownPage &&
+            (
+              editing ?
+              <button onClick={() => {
+                updateDescription();
+                setEditing(false);
+              }}>
+                <SaveIcon />
+              </button> :
+              <button onClick={() => {
+                if (description === undefined) {
+                  setDescription(userData.description ?? '');
+                }
+                setEditing(true);
+              }}>
+                <EditIcon />
+              </button>
+            )
+          }
+        </div>
+      </div>
+      {
+        !projects ?
+        <Loading /> :
+        !projects.length ?
+        <p>No projects yet</p> :
+        <div className={styles.main}>
+          <p>Featured Project</p>
+          {
+            ownPage &&
+            <select
+              value={feat}
+              onChange={e => {
+                const project = e.target.value;
+                setFeatured(project);
+                updateFeatured(project);
+              }}
+            >
+              <option value=""></option>
+              {
+                projects.map(project =>
+                  <option
+                    value={project.id}
+                    key={project.id}
+                  >
+                    {project.title}
+                  </option>
+                )
+              }
+            </select>
+          }
+          {
+            !feat ?
+            <p>No project featured</p> :
+            projects
+            .filter(project => project.id === feat)
+            .map(project =>
+              <Project {...project} key={project.id} />
+            )
+          }
+          <div className={styles.projects}>
+            {
+              projects ?
+              !projects.length ?
+              <p>No projects yet</p> :
+              projects.map(project =>
+                <Project {...project} key={project.id} />
+              ) :
+              <Loading />
+            }
+          </div>
+        </div>
+      }
     </div>
   );
 }
