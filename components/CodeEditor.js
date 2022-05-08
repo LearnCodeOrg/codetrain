a2c7ff63f6e1d791c1dfa3a6be759ef598acc5c6
Cooper Saye <coopersaye@gmail.com>
2022-05-08 01:44:45 -0700

    Import styles.

--- components/CodeEditor.js
+++ components/CodeEditor.js
@@ -5,10 +5,18 @@ import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import MatButton from '../components/MatButton';
 
+import { useState } from 'react';
+
 import 'ace-builds/src-noconflict/mode-javascript.js';
 import 'ace-builds/src-noconflict/theme-monokai.js';
+import 'ace-builds/src-noconflict/theme-tomorrow.js';
 import 'ace-builds/src-noconflict/ext-language_tools';
 import 'ace-builds/src-noconflict/ext-searchbox';
+
+import styles from '../styles/components/CodeEditor.module.css';
+
+const maxFontSize = 48;
+const minFontSize = 6;
 
 export default function CodeEditor(props) {
   return (
