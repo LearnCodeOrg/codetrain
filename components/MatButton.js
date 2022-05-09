a7fb31984d4217fffe55427f8d7b6d182c183b01
Cooper Saye <coopersaye@gmail.com>
2022-05-09 13:24:13 -0700

    Return base tooltip.

--- components/MatButton.js
+++ components/MatButton.js
@@ -3,5 +3,11 @@ import Tooltip from './Tooltip';
 import styles from '../styles/components/MatButton.module.css';
 
 export default function MatButton(props) {
+  const { Icon, tooltip, onClick, className } = props;
+
+  return (
+    tooltip ?
+    <Tooltip title={tooltip}>
+    </Tooltip> :
   );
 }
