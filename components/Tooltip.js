5c98840d6a1d16f401c88526e79526119e23d132
Cooper Saye <coopersaye@gmail.com>
2022-05-09 13:24:13 -0700

    Populate tooltip component.

--- components/Tooltip.js
+++ components/Tooltip.js
@@ -1,4 +1,11 @@
 import TooltipBase from '@mui/material/Tooltip';
 
 export default function Tooltip(props) {
+  const { title } = props;
+
+  return (
+    <TooltipBase title={title} arrow disableInteractive>
+      {props.children}
+    </TooltipBase>
+  );
 }
